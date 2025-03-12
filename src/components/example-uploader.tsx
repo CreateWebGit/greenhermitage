abf3227ccd578f484e6f2103806e1aa127c02d2c
CreateWebGit <CreateWebGit@users.noreply.github.com>
2025-03-12 11:47:55 +0100

    some bugfix

--- src/components/example-uploader.tsx
+++
@@ -1,7 +0,0 @@
-import { UploadDropzone } from "@uploadthing/react";
-
-import { OurFileRouter } from "src/app/api/uploadthing/core";
-
-export const OurUploadDropzone = () => (
-  <UploadDropzone endpoint="withoutMdwr" />
-);
