62980a4cb6f4718690ecd25e62732bbf30821342
CreateWebGit <hej@createweb.se>
2025-07-22 18:03:11 +0200

    added metatags and favicon

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -1,6 +1,34 @@
 import Context from "../../context/context";
 import Footer from "@/components/UI/Footer";
 
+// app/layout.tsx
+
+export const metadata = {
+  title: "Green Hermitage - Vegetarisk restaurang i Gamla Stan, Stockholm",
+  description: "Vegetarisk mat lagad med kärlek i en trevlig miljö i Gamla Stan i Stockholm.",
+  metadataBase: new URL("https://greenhermitage.se"),
+  openGraph: {
+    title: "Green Hermitage - Vegetarisk restaurang i Gamla Stan",
+    description: "Vegetarisk mat lagad med kärlek i en trevlig miljö i Gamla Stan.",
+    url: "https://greenhermitage.se",
+    siteName: "Green Hermitage",
+    locale: "sv_SE",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Green Hermitage - Vegetarisk restaurang i Gamla Stan",
+    description: "Vegetarisk mat lagad med kärlek i en trevlig miljö i Gamla Stan.",
+  },
+  alternates: {
+    canonical: "/"
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
+};
+
 export default function Layout({ children }) {
   return (
     <div className="h-full">
